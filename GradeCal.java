052222d7d78eb0b824162abe7efd65afa071f508
apex2102 <148197237+apex2102@users.noreply.github.com>
2024-01-29 22:23:06 +0530

    Add files via upload

---
+++ GradeCal.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class GradeCal {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the number of subjects:");
+        int numSubjects = scanner.nextInt();
+
+        int totalMarks = 0;
+        int maxMarksPerSubject = 100;
+
+
+        for (int i = 1; i <= numSubjects; i++) {
+            System.out.print("Enter marks for Subject " + i + " (out of 100): ");
+            int marks = scanner.nextInt();
+
+            if (marks < 0 || marks > maxMarksPerSubject) {
+                System.out.println("Invalid input! Marks should be between 0 and 100. Please try again.");
+                i--;
+            } else {
+                totalMarks += marks;
+            }
+        }
+
+        double averagePercentage = (double) totalMarks / (numSubjects * maxMarksPerSubject) * 100;
+
+        char grade;
+        if (averagePercentage >= 90) {
+            grade = 'A';
+        } else if (averagePercentage >= 80) {
+            grade = 'B';
+        } else if (averagePercentage >= 70) {
+            grade = 'C';
+        } else if (averagePercentage >= 60) {
+            grade = 'D';
+        } else {
+            grade = 'F';
+        }
+
+        System.out.println("\nResults:");
+        System.out.println("Total Marks: " + totalMarks);
+        System.out.println("Average Percentage: " + averagePercentage + "%");
+        System.out.println("Grade: " + grade);
+
+        scanner.close();
+    }
+}
+
